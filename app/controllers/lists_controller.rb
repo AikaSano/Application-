30a8ea1903f1282ebc774568467060a5d10d229c
AikaSano <127922906+AikaSano@users.noreply.github.com>
2023-03-21 23:14:26 +0000

    [Finish]2-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,12 @@
 class ListsController < ApplicationController
   def new
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list= List.new
+    @list = List.new
   end
 
   def create
     # 1.&2. データを受け取り新規登録するためのインスタンス作成
-    list =List.new(list_params)
+    list = List.new(list_params)
     # 3.データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
@@ -14,6 +14,7 @@ class ListsController < ApplicationController
   end  
   
   def index
+    @lists = List.all
   end
 
   def show
